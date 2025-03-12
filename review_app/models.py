cb666ba742266d2f7f6b0c2094b9c8e7fa804dd7
bethea28 <bryanbethea28@gmail.com>
2025-03-12 14:41:17 -0400

    add review working front and back

--- review_app/models.py
+++ review_app/models.py
@@ -2,3 +2,4 @@ from django.db import models
 
 class Review(models.Model):
   review = models.CharField(max_length=255)
+  rating = models.FloatField(default=0.0)  # Set the default rating to 0.0
--- review_app/views.py
+++ review_app/views.py
@@ -12,19 +12,25 @@ def addReview(request):
             # Attempt to parse JSON first
             data = json.loads(request.body)
             review = data.get('review')
+            rating = data.get('rating')
             # author = data.get('author')
             # year = data.get('year')
 
         except json.JSONDecodeError:
             # If JSON parsing fails, try request.POST (form data)
             review = request.POST.get('review')
+            rating = request.POST.get('rating')
             # author = request.POST.get('author')
             # year = request.POST.get('year')
 
         if not review:
             return JsonResponse({"error": "Title is required"}, status=400)
+        
+        if not rating:
+            return JsonResponse({"error": "Rating is required"}, status=400)
 
-        reviews = Review(review=review)
+        reviews = Review(review=review, rating= rating)
+        print('reviews pring now', reviews)
         reviews.save()
         return JsonResponse({"message": "review created!"})
 
