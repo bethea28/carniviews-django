7be30c43d4126a609f6b02e0563b377fc2913566
bethea28 <bryanbethea28@gmail.com>
2025-03-30 04:22:39 -0400

    review clean

--- review_app/urls.py
+++ review_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addReview/', views.addReview, name='addReview'),
+    # path('addReview/', views.addReview, name='addReview'),
     path('getReviews/', views.getReviews, name='getReviews'),
     path('getRatings/', views.getRatings, name='getRating'),
     path('<int:user_id>/<int:company_id>/addReview/', views.addReview, name='addReview'),
