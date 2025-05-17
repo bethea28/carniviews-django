dabd12c8141a95a11333978f0bc698db2e201890
bethea28 <bryanbethea28@gmail.com>
2025-05-17 04:28:37 -0400

    admin events

--- band_story_app/admin.py
+++ band_story_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import BandStory
 
-# Register your models here.
+@admin.register(BandStory)
+class BandStoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'company', 'claps')
+    search_fields = ('name', 'intro', 'vibe', 'costume', 'moments', 'reflection')
+    list_filter = ('company', 'user')
+    readonly_fields = ('claps',)
+    ordering = ('-claps',)
+
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'company', 'name', 'photos')
+        }),
+        ('Story Details', {
+            'fields': ('intro', 'vibe', 'costume', 'moments', 'reflection'),
+            'classes': ('collapse',),
+        }),
+        ('Engagement', {
+            'fields': ('claps',),
+        }),
+    )
