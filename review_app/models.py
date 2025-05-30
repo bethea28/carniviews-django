d47c6369a3ae0d76a7546e77409a9aea5b135378
bethea28 <bethea28@bethea28s-air.myfiosgateway.com>
2025-05-30 01:01:36 -0400

    review overall

--- review_app/models.py
+++ review_app/models.py
@@ -18,6 +18,7 @@ class Review(models.Model):
     value = models.FloatField(default=0.0, null=True)
     amenities = models.FloatField(default=0.0, null=True)
     pickup = models.FloatField(default=0.0, null=True)
+    overall = models.FloatField(default=0.0, null=True)
     rating = JSONField(default=list, blank=True, null=True)
     review_date = models.DateTimeField(auto_now_add=True)
 
